a76526d8932beb6ecd3b69218d3dd7c766888b3e
Allen Boynton <aboynton@fullsail.edu>
2016-09-16 22:27:46 -0400

    s

--- Sky Escape/SkyEscape/SkyEscape/AppDelegate.swift
+++ Sky Escape/SkyEscape/SkyEscape/AppDelegate.swift
@@ -11,6 +11,8 @@ import UIKit
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
+//    print(UIFont.familyNames())
+    
     var window: UIWindow?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
