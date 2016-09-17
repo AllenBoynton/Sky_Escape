94b7beb34d0fe61ffbbd1cf50fac9a7c313032af
Allen Boynton <aboynton@fullsail.edu>
2016-09-16 22:26:58 -0400

    s

--- Sky Escape/SkyEscape/SkyEscape/MainMenu.swift
+++ Sky Escape/SkyEscape/SkyEscape/MainMenu.swift
@@ -10,40 +10,46 @@ import SpriteKit
 
 class MainMenu: SKScene {
     
-    var infoButton = SKSpriteNode()
-    var node = SKNode()
-    
     override func didMoveToView(view: SKView) {
+        
+        self.addChild(self.infoButtonNode())
         
         self.runAction(SKAction.playSoundFileNamed("bgMusic", waitForCompletion: true))
     }
     
+    // Info button to show credits
+    func infoButtonNode() -> SKSpriteNode {
+        
+        let infoNode: SKSpriteNode = SKSpriteNode(imageNamed: "info")
+        infoNode.position = CGPoint(x: self.frame.width - infoNode.size.width / 2, y: self.frame.height - infoNode.size.height / 2)
+        infoNode.size = CGSize(width: 95, height: 95)
+        infoNode.zPosition = 5
+        infoNode.name = "infoButtonNode"
+        
+        return infoNode
+    }
+
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         
         for touch: AnyObject in touches {
             let location = (touch as! UITouch).locationInNode(self)
             
-            // Info button to show credits
-            infoButton = SKScene(fileNamed: "MainMenu")!.childNodeWithName("info") as! SKSpriteNode
-            infoButton.zPosition = 1000
-            infoButton.name = "InfoButton"
-            
-            infoButton.removeFromParent()
-            self.addChild(infoButton)
-            
             let node = self.nodeAtPoint(location)
-            if (node.name == "InfoButton") {
+            
+            if (node.name! == "infoButtonNode") {
                 
                 let info: CreditsScene = CreditsScene(fileNamed: "CreditsScene")!
                 info.scaleMode = .AspectFit
-                let transition: SKTransition = SKTransition.doorsOpenHorizontalWithDuration(3.0)
+                let transition: SKTransition = SKTransition.doorsOpenHorizontalWithDuration(2.0)
                 self.view?.presentScene(info, transition: transition)
             }
-            
-            let game: InstructionScene = InstructionScene(fileNamed: "InstructionScene")!
-            game.scaleMode = .AspectFit
-            let transition: SKTransition = SKTransition.doorsOpenHorizontalWithDuration(3.0)
-            self.view?.presentScene(game, transition: transition)
+            else {
+                
+                let game: InstructionScene = InstructionScene(fileNamed: "InstructionScene")!
+                game.scaleMode = .AspectFit
+                let transition: SKTransition = SKTransition.doorsOpenHorizontalWithDuration(3.0)
+                self.view?.presentScene(game, transition: transition)
+            }
         }
     }
 }
